fca141b9a97ae3e9fb07f95424de6ce2e1f9e08b
L <ivanrobleslazaro@gmail.com>
2024-11-04 08:29:24 -0600

    fallan las rotaciones y algunos colores

--- OpenGLProject1/OpenGLProject1/main.cpp
+++ OpenGLProject1/OpenGLProject1/main.cpp
@@ -1,14 +1,17 @@
 #include <GL/glut.h>
 #include <iostream>
+#include <cmath>
 
-float rotationAngle = .0f;  
-bool rotatingx = false;       
-bool rotatingy = false;       
-bool rotatingz = false;     
-float depthOffset = 0;  
-int rotatingColumn = -1;
-int rotatingRow = -1;
-int rotatingColumn2 = -1;
+float rotationAngle = 0.0f;
+bool rotating = false;
+int rotatingAxis = -1; // 0: X-axis, 1: Y-axis, 2: Z-axis
+int rotatingLayer = 0;
+
+struct CubeRotation {
+    float x, y, z;
+};
+
+CubeRotation cubeRotations[3][3][3];
 
 void initGL() {
     glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
@@ -17,80 +20,98 @@ void initGL() {
     glDepthFunc(GL_LEQUAL);
     glShadeModel(GL_SMOOTH);
     glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+    for (int x = 0; x < 3; ++x)
+        for (int y = 0; y < 3; ++y)
+            for (int z = 0; z < 3; ++z)
+                cubeRotations[x][y][z] = { 0.0f, 0.0f, 0.0f };
 }
 
-void drawCube(float x, float y, float z) {
+void drawCube(float x, float y, float z, CubeRotation rotation) {
     glPushMatrix();
-    glTranslatef(x, y, z + depthOffset); 
+    glTranslatef(x, y, z);
+
+    glRotatef(rotation.x, 1.0f, 0.0f, 0.0f);
+    glRotatef(rotation.y, 0.0f, 1.0f, 0.0f);
+    glRotatef(rotation.z, 0.0f, 0.0f, 1.0f);
 
     glBegin(GL_QUADS);
+    float s = 0.5f;
 
-    float s = 1.0f;
-
-    glColor3f(0.0f, 1.0f, 0.0f);
+    glColor3f(0.0f, 1.0f, 0.0f); // Green face
     glVertex3f(s, s, -s); glVertex3f(-s, s, -s); glVertex3f(-s, s, s); glVertex3f(s, s, s);
 
-    glColor3f(1.0f, 0.5f, 0.0f);
+    glColor3f(1.0f, 0.5f, 0.0f); // Orange face
     glVertex3f(s, -s, s); glVertex3f(-s, -s, s); glVertex3f(-s, -s, -s); glVertex3f(s, -s, -s);
 
-    glColor3f(1.0f, 0.0f, 0.0f);
+    glColor3f(1.0f, 0.0f, 0.0f); // Red face
     glVertex3f(s, s, s); glVertex3f(-s, s, s); glVertex3f(-s, -s, s); glVertex3f(s, -s, s);
 
-    glColor3f(1.0f, 1.0f, 0.0f);
+    glColor3f(1.0f, 1.0f, 0.0f); // Yellow face
     glVertex3f(s, -s, -s); glVertex3f(-s, -s, -s); glVertex3f(-s, s, -s); glVertex3f(s, s, -s);
 
-    glColor3f(0.0f, 0.0f, 1.0f);
+    glColor3f(0.0f, 0.0f, 1.0f); // Blue face
     glVertex3f(-s, s, s); glVertex3f(-s, s, -s); glVertex3f(-s, -s, -s); glVertex3f(-s, -s, s);
 
-    glColor3f(0.1f, 0.0f, 0.1f);
+    glColor3f(0.1f, 0.0f, 0.1f); // Purple face
     glVertex3f(s, s, -s); glVertex3f(s, s, s); glVertex3f(s, -s, s); glVertex3f(s, -s, -s);
     glEnd();
 
     glPopMatrix();
 }
 
+void updateCubeOrientation(int axis, int layer) {
+    for (int x = 0; x < 3; ++x) {
+        for (int y = 0; y < 3; ++y) {
+            for (int z = 0; z < 3; ++z) {
+                if ((axis == 0 && x == layer) ||
+                    (axis == 1 && y == layer) ||
+                    (axis == 2 && z == layer)) {
+
+                    CubeRotation& rotation = cubeRotations[x][y][z];
+
+                    switch (axis) {
+                    case 0: rotation.x += 90.0f; break;
+                    case 1: rotation.y += 90.0f; break;
+                    case 2: rotation.z += 90.0f; break;
+                    }
+                }
+            }
+        }
+    }
+}
+
 void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
+    gluLookAt(3.0, 3.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
-    float spacing = 2.3f;
+    float spacing = 1.1f;
 
-    for (int x = -1; x <= 1; ++x) {
-        for (int y = -1; y <= 1; ++y) {
-            for (int z = -1; z <= 1; ++z) {
-                if (rotatingx && rotatingColumn == x && !rotatingy && !rotatingz) {
-                    
+    for (int x = 0; x < 3; ++x) {
+        for (int y = 0; y < 3; ++y) {
+            for (int z = 0; z < 3; ++z) {
+                CubeRotation rotation = cubeRotations[x][y][z];
+
+                if (rotating && ((rotatingAxis == 0 && x == rotatingLayer) ||
+                    (rotatingAxis == 1 && y == rotatingLayer) ||
+                    (rotatingAxis == 2 && z == rotatingLayer))) {
+
                     glPushMatrix();
-                    glTranslatef(x * spacing, 0.0f, 0.0f);
-                    glRotatef(rotationAngle, 1.0f, 0.0f, 0.0f);
-                    glTranslatef(-x * spacing, 0.0f, 0.0f);
-                    drawCube(x * spacing, y * spacing, z * spacing);
-                    glPopMatrix();
-                }
+                    if (rotatingAxis == 0) glRotatef(rotationAngle, 1.0f, 0.0f, 0.0f);
+                    if (rotatingAxis == 1) glRotatef(rotationAngle, 0.0f, 1.0f, 0.0f);
+                    if (rotatingAxis == 2) glRotatef(rotationAngle, 0.0f, 0.0f, 1.0f);
 
-                else if (rotatingy && rotatingRow == y && !rotatingx && !rotatingz) {
-                    
-                    glPushMatrix();
-                    glTranslatef(0.0f, y * spacing, 0.0f);
-                    glRotatef(rotationAngle, 0.0f, 1.0f, 0.0f);
-                    glTranslatef(0.0f, -y * spacing, 0.0f);
-                    drawCube(x * spacing, y * spacing, z * spacing);
+                    glTranslatef((x - 1) * spacing, (y - 1) * spacing, (z - 1) * spacing);
+                    drawCube(0.0f, 0.0f, 0.0f, rotation);
                     glPopMatrix();
-                }
-
-                else if (rotatingz && rotatingColumn2 == z && !rotatingx && !rotatingy) {
-                    
-                    glPushMatrix();
-                    glTranslatef(0.0f,  0.0f, z * spacing);
-                    glRotatef(rotationAngle, 0.0f, 0.0f, 1.0f);
-                    glTranslatef(0.0f,  0.0f, - z * spacing);
-                    drawCube(x * spacing, y * spacing, z * spacing);
-                    glPopMatrix();
-                    
                 }
                 else {
-                    drawCube(x * spacing, y * spacing, z * spacing);
+                    glPushMatrix();
+                    glTranslatef((x - 1) * spacing, (y - 1) * spacing, (z - 1) * spacing);
+                    drawCube(0.0f, 0.0f, 0.0f, rotation);
+                    glPopMatrix();
                 }
             }
         }
@@ -99,17 +120,16 @@ void display() {
 }
 
 void timer(int value) {
-    if (rotatingx || rotatingy || rotatingz) {
-        rotationAngle += 2.5f;
+    if (rotating) {
+        rotationAngle += 5.0f;
         if (rotationAngle >= 90.0f) {
             rotationAngle = 0.0f;
-            rotatingx = false;
-            rotatingy = false;
-            rotatingz = false;
+            rotating = false;
+            updateCubeOrientation(rotatingAxis, rotatingLayer);
         }
     }
     glutPostRedisplay();
-    glutTimerFunc(30, timer, 0);
+    glutTimerFunc(16, timer, 0);
 }
 
 void reshape(GLsizei width, GLsizei height) {
@@ -119,28 +139,27 @@ void reshape(GLsizei width, GLsizei height) {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluPerspective(45.0f, aspect, .1f, 100.0f);
-    gluLookAt(0.0, 0.0, 15.0,
-        0.0, 0.0, 0.0,
-        0.0, 1.0, 0.0);
 }
 
 void keyboard(unsigned char key, int x, int y) {
-    switch (key) {
-    case '1': rotatingColumn = -1; rotatingx = true; break; // Columna izquierda
-    case '2': rotatingColumn = 0; rotatingx = true; break;  // Columna central
-    case '3': rotatingColumn = 1; rotatingx = true; break;  // Columna derecha
-    case '4': rotatingRow = -1; rotatingy = true; break; // Fila izquierda
-    case '5': rotatingRow = 0; rotatingy = true; break;  // Fila central
-    case '6': rotatingRow = 1; rotatingy = true; break;  // Fila derecha
-    case '7': rotatingColumn2 = -1; rotatingz = true; break; // Columna 2 izquierda
-    case '8': rotatingColumn2 = 0; rotatingz = true; break;  // Columna 2 central
-    case '9': rotatingColumn2 = 1; rotatingz = true; break;  // Columna 2 derecha
+    if (!rotating) {
+        switch (key) {
+        case '1': rotatingAxis = 0; rotatingLayer = 0; rotating = true; break; // X-axis left
+        case '2': rotatingAxis = 0; rotatingLayer = 1; rotating = true; break; // X-axis middle
+        case '3': rotatingAxis = 0; rotatingLayer = 2; rotating = true; break; // X-axis right
+        case '4': rotatingAxis = 1; rotatingLayer = 0; rotating = true; break; // Y-axis bottom
+        case '5': rotatingAxis = 1; rotatingLayer = 1; rotating = true; break; // Y-axis middle
+        case '6': rotatingAxis = 1; rotatingLayer = 2; rotating = true; break; // Y-axis top
+        case '7': rotatingAxis = 2; rotatingLayer = 0; rotating = true; break; // Z-axis front
+        case '8': rotatingAxis = 2; rotatingLayer = 1; rotating = true; break; // Z-axis middle
+        case '9': rotatingAxis = 2; rotatingLayer = 2; rotating = true; break; // Z-axis back
+        }
     }
 }
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(640, 480);
     glutInitWindowPosition(50, 50);
     glutCreateWindow("Rubik's Cube");
